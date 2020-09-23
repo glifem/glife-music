04ef8cbd480a40603b8fa1c205470309ce858bcf
William Desgardin <37619478+wdesgardin@users.noreply.github.com>
2020-09-24 01:13:57 +0200

    feat: music player (#6)
    * feat: setup react-native-track-player
    
    * feat: player screen interactions
    
    * feat: set player queue on music press
    
    * fix: play button cropped on ios devices
    
    * feat: player header translations, hide on ios
    
    * feat(ios): enable background mode
    
    * style: 4 spaces indent

--- app/models/music-store/music-store.ts
+++ app/models/music-store/music-store.ts
@@ -1,53 +1,49 @@
-import { types, Instance } from 'mobx-state-tree'
-import { flow } from 'mobx'
-import { withStatus } from '../extensions/with-status'
-import { withEnvironment } from '../extensions/with-environment'
-import { MusicClassModel, MusicClassModelType } from './music-models'
+import { types, Instance } from "mobx-state-tree"
+import { flow } from "mobx"
+import { withStatus } from "../extensions/with-status"
+import { withEnvironment } from "../extensions/with-environment"
+import { MusicClassModel, MusicClassModelType } from "./music-models"
 
 export const MusicModel = types
-	.model('MusicStore')
-	.props({
-		list: types.optional(types.array(MusicClassModel), []),
-		currentMusicId: types.number
-	})
-	.extend(withStatus)
-	.extend(withEnvironment)
-	.actions((self) => ({
-		setMusics(values: MusicClassModelType[]) {
-			if (self.list) {
-				if (values) {
-					self.list.replace(values as MusicClassModelType[])
-				} else {
-					self.list.clear()
-				}
-			} else {
-				// eslint-disable-next-line
-				self.list = values as any
-			}
-		},
-		setCurrentMusic(id: number) {
-			self.currentMusicId = id;
-		}
-	}))
-	.actions((self) => ({
-		getMusics: flow(function* () {
-			self.setStatus('pending')
-			try {
-				const result = yield self.environment.api.getMusics()
-				if (result.kind === 'ok') {
-					self.setMusics(result.musics)
-					self.setStatus('done')
-				} else {
-					self.setStatus('error')
-				}
-			} catch (e) {
-				console.log(e)
-				self.setStatus('error')
-			}
-		}),
-		getCurrentMusic: () => {
-			return self.list.find(music => music.id === self.currentMusicId)
-		}
-	}))
+    .model("MusicStore")
+    .props({
+        list: types.optional(types.array(MusicClassModel), []),
+    })
+    .extend(withStatus)
+    .extend(withEnvironment)
+    .actions((self) => ({
+        setMusics(values: MusicClassModelType[]) {
+            if (self.list) {
+                if (values) {
+                    self.list.replace(values as MusicClassModelType[])
+                } else {
+                    self.list.clear()
+                }
+            } else {
+                // eslint-disable-next-line
+                self.list = values as any
+            }
+        },
+    }))
+    .actions((self) => ({
+        getMusics: flow(function* () {
+            self.setStatus("pending")
+            try {
+                const result = yield self.environment.api.getMusics()
+                if (result.kind === "ok") {
+                    self.setMusics(result.musics)
+                    self.setStatus("done")
+                } else {
+                    self.setStatus("error")
+                }
+            } catch (e) {
+                console.log(e)
+                self.setStatus("error")
+            }
+        }),
+        getMusicById: (id: number) => {
+            return self.list.find((music) => music.id === id)
+        },
+    }))
 
 export type MusicStore = Instance<typeof MusicModel>
---
+++ app/utils/track-player.ts
@@ -0,0 +1,74 @@
+import { MusicClassModelType } from "../models/music-store/music-models"
+import TrackPlayer, {
+    Event,
+    State,
+    Track,
+    usePlaybackState,
+    useTrackPlayerEvents,
+} from "react-native-track-player"
+import { Platform } from "react-native"
+import { getMusicAuthorNames } from "../models/music-store/music-selector"
+import { useEffect, useState } from "react"
+const { S3_URL } = require("../config/env")
+
+export const setPlayerQueue = async (
+    musics: MusicClassModelType[],
+    startWithId: number | string,
+) => {
+    await TrackPlayer.reset()
+    await TrackPlayer.setupPlayer()
+
+    const tracks: Track[] = musics.map((music) => ({
+        id: music.id.toString(),
+        title: music.title,
+        artist: getMusicAuthorNames(music),
+        artwork: `${S3_URL}/music/${music.id}.png`,
+        //ios doesn't support ogg and mp3 files aren't available yet
+        url:
+            Platform.OS == "android"
+                ? `${S3_URL}/music/${music.id}.ogg`
+                : "https://cdn.discordapp.com/attachments/392980227263037450/758085951963529267/80.mp3",
+    }))
+
+    await TrackPlayer.add(tracks)
+    await TrackPlayer.skip(startWithId.toString())
+    await TrackPlayer.play()
+}
+
+export const useCurrentTrack = () => {
+    const [trackId, setTrackId] = useState<string>(null)
+    const [track, setTrack] = useState<Track>(null)
+
+    useTrackPlayerEvents([Event.PlaybackTrackChanged], ({ nextTrack }) => {
+        setTrackId(nextTrack)
+    })
+
+    useEffect(() => {
+        let didCancel = false
+        TrackPlayer.getCurrentTrack().then((trackId) => {
+            !didCancel && trackId && setTrackId(trackId)
+        })
+        return () => (didCancel = true)
+    }, [])
+
+    useEffect(() => {
+        let didCancel = false
+        TrackPlayer.getTrack(trackId)
+            .then((track) => !didCancel && setTrack(track))
+            .catch(() => !didCancel && setTrack(null))
+        return () => (didCancel = true)
+    }, [trackId])
+
+    return track
+}
+
+export const useIsPlaying = () => {
+    const [playing, setPlaying] = useState(false)
+    const state = usePlaybackState()
+
+    useEffect(() => {
+        setPlaying(state == State.Playing || state == State.Buffering)
+    }, [state])
+
+    return playing
+}
